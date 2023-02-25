633dbd04aa8570ea142fce8a003b2352c772fc26
qwerty7989 <44471969+qwerty7989@users.noreply.github.com>
2023-02-25 20:18:13 +0700

    Add GameObject

---
+++ PuzzleBobbleHell/Objects/GameObject.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleBobbleHell.GameObject
+{
+    internal interface GameObject
+    {
+        public void LoadContent(ContentManager Content);
+        public void UnloadContent();
+        public void Update(GameTime gameTime);
+        public void Draw(SpriteBatch spriteBatch);
+    }
+}
