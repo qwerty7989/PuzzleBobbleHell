203db12312ee6b69a64b52b3cfec4f03b8d0c2e7
Saint99599 <82707210+Saint99599@users.noreply.github.com>
2023-03-03 14:17:30 +0700

    edit : EndStageScene

--- PuzzleBobbleHell/Scenes/EndStageScene.cs
+++ PuzzleBobbleHell/Scenes/EndStageScene.cs
@@ -1,21 +1,46 @@
 ﻿using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PuzzleBobbleHell.Objects;
 
 namespace PuzzleBobbleHell.Scenes
 {
     public class EndStageScene : GameScene
     {
+
         private ContentManager contentManager;
+
+        private Button nextButton;
+
+        private Image backgroundImage;
+
+        private Rectangle cursorRectangle;
+        private Texture2D cursorTexture;
+        private MouseState previousMouseState;
+
+        public EndStageScene()
+        {
+            cursorRectangle = new Rectangle(0, 0, 100, 100);
+        }
+
         public void LoadContent(ContentManager Content)
         {
             contentManager = new ContentManager(Content.ServiceProvider, Content.RootDirectory);
+
+            backgroundImage = new Image(new Rectangle(0, 0, Singleton.Instance.widthScreen, Singleton.Instance.heightScreen), contentManager.Load<Texture2D>("MenuScene/background"));
+            nextButton = new Button(new Rectangle((Singleton.Instance.widthScreen / 2) - 150, 500, 300, 75), contentManager.Load<Texture2D>("MenuScene/play_button"));
+            cursorTexture = contentManager.Load<Texture2D>("MenuScene/cursor");
+
+            /* Subscribe Event */
+            nextButton.OnClicked += NextButtonAction;
         }
+
         public void UnloadContent()
         {
             contentManager.Unload();
@@ -23,15 +48,30 @@ namespace PuzzleBobbleHell.Scenes
 
         public void Update(GameTime gameTime)
         {
-            // TODO: Add your update logic here
+            /* Cursor Handle */
+            MouseState currentMouseState = Mouse.GetState();
+            cursorRectangle.X = currentMouseState.X;
+            cursorRectangle.Y = currentMouseState.Y;
+            previousMouseState = currentMouseState;
 
-            // ? Change scene Example
-            // ? Singleton.Instance.sceneManager.changeScene(Manager.SceneManager.SceneName.MenuScene);
+            // TODO: update as list
+            nextButton.Update(gameTime);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            // TODO: Add your drawing code here
+            // TODO: draw as list
+            backgroundImage.Draw(spriteBatch);
+            nextButton.Draw(spriteBatch);
+
+            spriteBatch.Draw(cursorTexture, cursorRectangle, Color.White);
+        }
+
+        private void NextButtonAction()
+        {
+            // TODO: add feedback
+
+            Singleton.Instance.sceneManager.changeScene(Manager.SceneManager.SceneName.EndGameScene);
         }
     }
 }
