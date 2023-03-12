8aa5a29628c9f30356d739080b04cf407e512737
Saint99599 <82707210+Saint99599@users.noreply.github.com>
2023-03-13 02:09:04 +0700

    End Stage Scene

--- PuzzleBobbleHell/Scenes/EndStageScene.cs
+++ PuzzleBobbleHell/Scenes/EndStageScene.cs
@@ -13,12 +13,19 @@ namespace PuzzleBobbleHell.Scenes
 {
     public class EndStageScene : GameScene
     {
+        int Score = Singleton.Instance.Score;
 
         private ContentManager contentManager;
 
+        private Button playButton;
         private Button nextButton;
 
         private Image backgroundImage;
+        private Image starfishImage;
+
+        private SpriteFont textFont;
+        private string paragraph;
+        private string score;
 
         private Rectangle cursorRectangle;
         private Texture2D cursorTexture;
@@ -27,17 +34,24 @@ namespace PuzzleBobbleHell.Scenes
         public EndStageScene()
         {
             cursorRectangle = new Rectangle(0, 0, 100, 100);
+
+            paragraph = "SCORE";
+            score = Score.ToString();
         }
 
         public void LoadContent(ContentManager Content)
         {
             contentManager = new ContentManager(Content.ServiceProvider, Content.RootDirectory);
 
-            backgroundImage = new Image(new Rectangle(0, 0, Singleton.Instance.widthScreen, Singleton.Instance.heightScreen), contentManager.Load<Texture2D>("MenuScene/background"));
-            nextButton = new Button(new Rectangle((Singleton.Instance.widthScreen / 2) - 150, 200, 300, 75), contentManager.Load<Texture2D>("MenuScene/play_button"));
+            backgroundImage = new Image(new Rectangle(0, 0, Singleton.Instance.widthScreen, Singleton.Instance.heightScreen), contentManager.Load<Texture2D>("EndStageScene/background1"));
+
+            playButton = new Button(new Rectangle((Singleton.Instance.widthScreen / 2) -200, 600, 75, 75), contentManager.Load<Texture2D>("MenuScene/play_button"));
+            nextButton = new Button(new Rectangle((Singleton.Instance.widthScreen / 2) -100, 600, 300, 75), contentManager.Load<Texture2D>("MenuScene/play_button"));
+            textFont = contentManager.Load<SpriteFont>("Font/Pixel");
             cursorTexture = contentManager.Load<Texture2D>("MenuScene/cursor");
 
             /* Subscribe Event */
+            playButton.OnClicked += PlayButtonAction;
             nextButton.OnClicked += NextButtonAction;
         }
 
@@ -55,16 +69,51 @@ namespace PuzzleBobbleHell.Scenes
             previousMouseState = currentMouseState;
 
             // TODO: update as list
+            playButton.Update(gameTime);
             nextButton.Update(gameTime);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            // TODO: draw as list
             backgroundImage.Draw(spriteBatch);
+
+            Texture2D starfishTexture;
+            if (int.TryParse(score.Replace(",", ""), out int scoreValue))
+            {
+                if (scoreValue < 5000)
+                    starfishTexture = contentManager.Load<Texture2D>("EndStageScene/starfish0");
+                else if (scoreValue < 8000)
+                    starfishTexture = contentManager.Load<Texture2D>("EndStageScene/starfish1");
+                else if (scoreValue < 10000)
+                    starfishTexture = contentManager.Load<Texture2D>("EndStageScene/starfish2");
+                else
+                    starfishTexture = contentManager.Load<Texture2D>("EndStageScene/starfish3");
+            }
+            else
+            {
+                starfishTexture = contentManager.Load<Texture2D>("EndStageScene/starfish2");
+            }
+
+            starfishImage = new Image(new Rectangle((Singleton.Instance.widthScreen / 2) - 250, 180, 500, 250), starfishTexture);
+            starfishImage.Draw(spriteBatch);
+
+            playButton.Draw(spriteBatch);
             nextButton.Draw(spriteBatch);
 
+            Vector2 paragraphPosition = new Vector2((Singleton.Instance.widthScreen / 2) - (textFont.MeasureString(paragraph).X / 2), (Singleton.Instance.heightScreen / 2) - (textFont.MeasureString(paragraph).Y / 2));
+            spriteBatch.DrawString(textFont, paragraph, paragraphPosition, Color.White);
+
+            Vector2 scorePosition = new Vector2((Singleton.Instance.widthScreen / 2) - (textFont.MeasureString(score).X / 2), (Singleton.Instance.heightScreen / 2) + 50);
+            spriteBatch.DrawString(textFont, score, scorePosition, Color.White);
+
             spriteBatch.Draw(cursorTexture, cursorRectangle, Color.White);
+        }
+
+        private void PlayButtonAction()
+        {
+            // TODO: add feedback
+
+            Singleton.Instance.sceneManager.changeScene(Manager.SceneManager.SceneName.MenuScene);
         }
 
         private void NextButtonAction()
